2aca12458e2ce56fa8a35699925c2ed4e733b0ce
Oleg Korsunsky <wd@dizaina.net>
2017-04-11 17:37:17 +0300

    Added Chatra account setting + other tweaks and improvements

--- app.js
+++ app.js
@@ -6,32 +6,26 @@
         if (!options.window) options.window = {};
         if (!options.advanced) options.advanced = {};
 
-        // var injectEl;
-        // if (options.advanced.inject && options.advanced.injectTo) {
-        //     injectEl = INSTALL.createElement(options.advanced.injectTo);
-        //     injectEl.style.display = 'block';
-        //     injectEl.style.height = options.advanced.injectHeight+'px';
-        // }
-
         var result = {
-            buttonStyle: options.button.style || void 0,
-            buttonSize: parseInt(options.button.size) || void 0,
-            buttonPosition: options.button.position || void 0,
             chatWidth: parseInt(options.window.width) || void 0,
             chatHeight: parseInt(options.window.height) || void 0,
             zIndex: options.advanced.zIndex !== null? options.advanced.zIndex: void 0,
             mobileOnly: options.advanced.devices == 'mob',
             disabledOnMobile: options.advanced.devices == 'notMob',
             language: options.advanced.language || void 0,
-            mode: options.advanced.inject && options.advanced.injectTo? 'frame': 'widget',
-            injectTo: options.advanced.injectTo? doc.querySelector(options.advanced.injectTo): void 0,
             groupId: options.advanced.groupId || void 0
         };
 
+        if (!options.button.useChatraSettings) {
+            result.buttonStyle = options.button.style || void 0;
+            result.buttonSize = parseInt(options.button.size) || void 0;
+            result.buttonPosition = options.button.position || void 0;
+        }
+
         result.colors = {};
 
-        if (options.button.textColor) result.colors.buttonText = options.button.textColor;
-        if (options.button.bgColor) result.colors.buttonBg = options.button.bgColor;
+        if (!options.button.useChatraSettings && options.button.textColor) result.colors.buttonText = options.button.textColor;
+        if (!options.button.useChatraSettings && options.button.bgColor) result.colors.buttonBg = options.button.bgColor;
         if (options.window.bgColor) result.colors.chatBg = options.window.bgColor;
         if (options.window.clientBubbleBg) result.colors.clientBubbleBg = options.window.clientBubbleBg;
         if (options.window.agentBubbleBg) result.colors.agentBubbleBg = options.window.agentBubbleBg;
@@ -42,7 +36,7 @@
     function initialize(options) {
         win.ChatraSetup = convertOptions(options);
 
-        win.ChatraID = options.chatraId;
+        win.ChatraID = options.account && options.account.userId;
 
         var s = doc.createElement('script');
         win.Chatra = win.Chatra || function() {
@@ -59,8 +53,8 @@
 
         console.info(newChatraSetup);
 
-        if (options.chatraId != win.ChatraID) {
-            win.ChatraID = options.chatraId;
+        if (!options.account || options.account.userId != win.ChatraID) {
+            win.ChatraID = options.account && options.account.userId;
             win.ChatraSetup = newChatraSetup;
             Chatra('restart');
         }
@@ -74,9 +68,7 @@
         else if (
             win.ChatraSetup.mobileOnly != newChatraSetup.mobileOnly ||
             win.ChatraSetup.disabledOnMobile != newChatraSetup.disabledOnMobile ||
-            win.ChatraSetup.language != newChatraSetup.language ||
-            win.ChatraSetup.mode != newChatraSetup.mode ||
-            win.ChatraSetup.injectTo != newChatraSetup.injectTo
+            win.ChatraSetup.language != newChatraSetup.language
         ) {
             var isExpanded = Chatra._chatExpanded;
 
